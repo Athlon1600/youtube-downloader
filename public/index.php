48ffaba21f7b030e0a40cb96b2b2cb336af9c259
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2020-06-16 08:02:23 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -91,7 +91,7 @@ header('Content-Type: application/json; charset=utf-8');
 
 $result = [
     'url' => $links[0]["url"],
-    'tot' => $links["bite"][0],
+    'tot' => $links[0]["bite"],
     'title' => $title,
     'view_count' => $view_count,
     'duration' => covtime($VidDuration),
