32b3f61b8367597be808fa0f81108cbd27203365
Athlon1600 <Athlon2400@gmail.com>
2021-07-26 12:25:41 -0500

    test getVideoInfo method across multiple devices because sometimes YouTube returns its data differently depending on your device

---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace YouTube\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    const UA_CHROME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+    const UA_FIREFOX = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0';
+    const UA_EDGE = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55';
+    const UA_OPERA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 OPR/77.0.4054.277';
+    const UA_SAFARI = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15';
+    const UA_IPHONE = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1';
+
+    // We assume that this video will never get taken down
+    const BUNNY_VIDEO_ID = 'https://www.youtube.com/watch?v=aqz-KE-bpKQ';
+}
--- tests/YouTubePageTest.php
+++ tests/YouTubePageTest.php
@@ -2,29 +2,36 @@
 
 namespace YouTube\Tests;
 
-use PHPUnit\Framework\TestCase;
 use YouTube\YouTubeDownloader;
 
 class YouTubePageTest extends TestCase
 {
-    const BUNNY_VIDEO_ID = "aqz-KE-bpKQ";
-
     public function test_get_video_info()
     {
         $youtube = new YouTubeDownloader();
 
-        $page = $youtube->getPage(self::BUNNY_VIDEO_ID);
+        $agents = [
+            self::UA_FIREFOX,
+            self::UA_EDGE,
+            self::UA_IPHONE
+        ];
 
-        $info = $page->getVideoInfo();
+        foreach ($agents as $agent) {
+            $youtube->getBrowser()->setUserAgent($agent);
 
-        // we should be able to parse at least these
-        $this->assertNotEmpty($info->id);
-        $this->assertNotEmpty($info->title);
-        $this->assertNotEmpty($info->description);
+            $page = $youtube->getPage(self::BUNNY_VIDEO_ID);
 
-        $this->assertNotEmpty($info->pageUrl);
-        $this->assertNotEmpty($info->uploadDate);
-        $this->assertNotEmpty($info->thumbnail);
+            $info = $page->getVideoInfo();
+
+            // we should be able to parse at least these
+            $this->assertNotEmpty($info->id);
+            $this->assertNotEmpty($info->title);
+            $this->assertNotEmpty($info->channelTitle);
+            $this->assertNotEmpty($info->description);
+
+            $this->assertNotEmpty($info->pageUrl);
+            $this->assertNotEmpty($info->uploadDate);
+            $this->assertNotEmpty($info->thumbnail);
+        }
     }
-
 }
